7417596c16a0dafa35908b619872421f991cc2bf
Will Vetter <vetter.william@gmail.com>
2020-09-01 04:59:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   for item in array do
     if item == value_to_find
-      return array[item].index
+      return array.index(item)
     end
   end
 end
